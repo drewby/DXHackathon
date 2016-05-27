a3dbda33e22d4b65e8f9dba618237b9eb6f6ea6b
Drew Robbins <drew@drewby.com>
2016-05-27 15:37:48 +0900

    reading sensor data with SPI

--- src/LinuxTempe/main.cpp
+++ src/LinuxTempe/main.cpp
@@ -1,3 +1,5 @@
+#include "BME280.h"
+#include <stdio.h>
 #include <wiringPi.h>
 
 // LED Pin - wiringPi pin 0 is BCM_GPIO 17.
@@ -5,20 +7,23 @@
 // when choosing a different pin number please use the BCM numbering, also
 // update the Property Pages - Build Events - Remote Post-Build Event command 
 // which uses gpio export for setup for wiringPiSetupSys
-#define	LED	17
+//#define	LED	17
 
 int main(void)
 {
-	wiringPiSetupSys();
-
-	pinMode(LED, OUTPUT);
+	int response = bme280_init(0);
+	float tempC = 0;
+	float pressure = 0;
+	float humidity = 0;
 
 	while (true)
 	{
-		digitalWrite(LED, HIGH);  // On
-		delay(500); // ms
-		digitalWrite(LED, LOW);	  // Off
-		delay(500);
+		bme280_read_sensors(&tempC, &pressure, &humidity);
+		printf("Temperature (C): %f \n", tempC);
+		printf("Pressure: %f \n", pressure);
+		printf("Humidity: %f \n", humidity);
+		delay(2000);
+	
 	}
 	return 0;
 }
